bfd77da8b6c8a08bcf7f075a4b0f80ac1124ef48
xiphodon <827789895@qq.com>
2017-03-07 17:28:14 +0800

    k均值算法

--- ML_demo_05.py
+++ ML_demo_05.py
@@ -52,14 +52,37 @@ def ML_02(votes):
     '''
     # 参数 n_clusters=2：分为两簇
     kmeans_model = KMeans(n_clusters=2, random_state=1)
-    # 训练算法
-    senator_distances = kmeans_model.fit_transform(votes.iloc[:,3:])
-    # 显示结果为每行对应的每条数据距每个簇的中心的距离
-    print(senator_distances)
 
+    # 训练算法(显示结果为每行对应的每条数据距每个簇的中心的距离)
+    senator_distances = kmeans_model.fit_transform(votes.iloc[:,3:])
+    # print(senator_distances)
+
+
+    # 聚类结果标签
+    labels = kmeans_model.labels_
+    # 交叉表（聚类结果标签为一个维度，“party”各党派为一个维度）
+    print(pd.crosstab(labels, votes["party"]))
+    #如表所示
+    #   party   D   I   R
+    #   row_0
+    #   0       41  2   0
+    #   1       3   0   54
+
+    # 筛选出簇标签为1且党派为D的那3位议员
+    democratic_outliers = votes[(labels == 1) & (votes["party"] == "D")]
+    print(democratic_outliers)
+
+    # 散点图，横标为距簇0的距离，纵标为距纵标的距离，c=labels颜色（color）按照标签簇划分
+    plt.scatter(x=senator_distances[:,0], y=senator_distances[:,1], c=labels)
+    plt.show()
+
+
+def ML_03(votes):
+    pass
 
 
 if __name__ == "__main__":
     votes = init()
     # ML_01(votes)
     ML_02(votes)
+    ML_03(votes)
