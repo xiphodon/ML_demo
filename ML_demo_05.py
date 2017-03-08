ef94ec51fa1f4f1f2e1db9abb5d41fbc3fc5d188
xiphodon <827789895@qq.com>
2017-03-08 11:40:18 +0800

    Kmeans算法分析nba球员数据

--- ML_demo_05.py
+++ ML_demo_05.py
@@ -17,15 +17,25 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import math
 
-def init():
+def init_01():
     '''
     数据初始化
     （美国国会投票数据）
     :return:
     '''
     votes = pd.read_csv(r"data/114_congress.csv")
-    # print(data.head())
+    # print(votes.head())
     return votes
+
+def init_02():
+    '''
+    数据初始化
+    （nba球员数据）
+    :return:
+    '''
+    nba = pd.read_csv(r"data/nba_2013.csv")
+    # print(nba.head())
+    return nba
 
 def ML_01(votes):
     '''
@@ -76,13 +86,68 @@ def ML_02(votes):
     plt.scatter(x=senator_distances[:,0], y=senator_distances[:,1], c=labels)
     plt.show()
 
+    # 计算离群点（距离值的平方和或立方和等等）
+    extremism = (senator_distances ** 3).sum(axis=1) # 立方后按行求和
+    votes["extremism"] = extremism # 增加离群点计算列“extremism”
+    votes.sort_values("extremism", inplace=True, ascending=False) # 替换原值，按照离群点离群距离降序排列
+    print(votes.head(10))
 
-def ML_03(votes):
-    pass
+
+def ML_03(nba):
+    '''
+    聚类分析nba球员数据
+    :param nba: nba球员数据
+    :return:
+    '''
+    # 选择nba中位置为控球后卫的球员数据
+    point_guards = nba[nba["pos"] == "PG"]
+    # pts：总得分数，g：比赛次数，ppg：平均每场得分数
+    point_guards["ppg"] = point_guards["pts"] / point_guards["g"]
+    # print(point_guards[["pts", "g", "ppg"]].head())
+
+    # tov：失误总数
+    point_guards = point_guards[point_guards["tov"] != 0]
+    # ast：助攻总数，atr：该球员贡献度的一种表示
+    point_guards["atr"] = point_guards["ast"] / point_guards["tov"]
+
+    plt.scatter(x=point_guards["ppg"], y=point_guards["atr"], c="y")
+    plt.title("Point Guards")
+    plt.xlabel("Points Per Game", fontsize=13)
+    plt.ylabel("Assist Turnover Ratio", fontsize=13)
+    plt.show()
+
+
+    # K-means聚类
+    num_clusters = 5 # 定义簇数
+    kmeans = KMeans(n_clusters=num_clusters) # 分5簇
+    kmeans.fit(point_guards[["ppg", "atr"]]) # 训练算法
+    point_guards["cluster"] = kmeans.labels_ # 生成新列，标记簇标签
+
+    def visualize_clusters(df, num_clusters):
+        '''
+        可视化各簇
+        :param df: 数据集
+        :param num_clusters: 分簇数
+        :return:
+        '''
+        colors = ["b","g","r","c","m","y","k"]
+
+        for n in range(num_clusters):
+            clustered_df = df[df["cluster"] == n]
+            plt.scatter(clustered_df["ppg"], clustered_df["atr"], c=colors[n])
+            plt.xlabel("Points Per Game", fontsize=13)
+            plt.ylabel("Assist Turnover Ratio", fontsize=13)
+
+        plt.show()
+
+    # 可视化各簇
+    visualize_clusters(point_guards, num_clusters)
+
 
 
 if __name__ == "__main__":
-    votes = init()
+    votes = init_01()
+    nba = init_02()
     # ML_01(votes)
-    ML_02(votes)
-    ML_03(votes)
+    # ML_02(votes)
+    ML_03(nba)
